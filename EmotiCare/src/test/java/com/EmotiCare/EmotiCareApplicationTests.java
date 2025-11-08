47983c37b2c8233172694e061db27aba5e18e1ff
Chekroun Adnan <adnanchekroun00@gmail.com>
2025-11-08 12:40:22 +0100

    first commit

---
+++ EmotiCare/src/test/java/com/EmotiCare/EmotiCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmotiCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotiCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
