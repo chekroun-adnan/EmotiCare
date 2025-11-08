dab6cf912fc06dca58761f8abb330d30ccc6605a
Chekroun Adnan <adnanchekroun00@gmail.com>
2025-11-08 15:20:38 +0100

    create user repository and a security configuration

---
+++ EmotiCare/src/main/java/com/EmotiCare/Repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.EmotiCare.Repositories;
+
+import com.EmotiCare.Entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
---
+++ EmotiCare/src/main/java/com/EmotiCare/Security/JwtUtil.java
@@ -0,0 +1,67 @@
+package com.EmotiCare.Security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${app.jwt.secret}")
+    private String secret;
+
+    public String getSecret() {
+        return secret;
+    }
+
+    private final long ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 15;
+    private final long REFRESH_TOKEN_EXPIRATION = 1000L * 60 * 60 * 24 * 7;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateAccessToken(String email) {
+        return generateToken(email, ACCESS_TOKEN_EXPIRATION);
+    }
+
+    public String generateRefreshToken(String email) {
+        return generateToken(email, REFRESH_TOKEN_EXPIRATION);
+    }
+
+    private String generateToken(String email, long expiration) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token) .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try{
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
