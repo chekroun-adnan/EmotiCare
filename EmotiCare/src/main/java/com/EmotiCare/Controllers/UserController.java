69ce886cbe5f0bfcc839df32ee261ee316f17418
Chekroun Adnan <adnanchekroun00@gmail.com>
2025-11-12 19:12:04 +0100

    fix database connection problem

--- EmotiCare/src/main/java/com/EmotiCare/Controllers/UserController.java
+++ EmotiCare/src/main/java/com/EmotiCare/Controllers/UserController.java
@@ -4,7 +4,7 @@ package com.EmotiCare.Controllers;
 import com.EmotiCare.Entities.User;
 import com.EmotiCare.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +17,7 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping("/me")
     public User getCurrentUser(Principal principal) {
         return userRepository.findByEmail(principal.getName())
                 .orElseThrow(()-> new RuntimeException("User Not Found"));
